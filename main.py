9632bab45cb416111e0a1a6e3ae6f4391440c85f
hyperfraise <damien.menigaux@polytechnique.edu>
2025-02-19 16:37:05 +0100

    s

--- main.py
+++ main.py
@@ -278,7 +278,7 @@ def train_model(data_dir, epochs=10, batch_size=BATCH_SIZE, lr=1e-4):
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8)
 
     model = TemporalUNetTransformer(NUM_CLASSES, num_frames=NUM_FRAMES).to(device)
-    model = torch.compile(model)
+    # model = torch.compile(model)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     criterion = nn.BCEWithLogitsLoss()
 
