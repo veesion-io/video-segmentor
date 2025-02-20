33ac1cb202339980d04c5d4baeb8001599cb553c
hyperfraise <damien.menigaux@polytechnique.edu>
2025-02-20 18:19:59 +0100

    s

--- visualize.py
+++ visualize.py
@@ -31,9 +31,7 @@ COLORS = [
 
 
 def load_model(model, checkpoint_path):
-    checkpoint = torch.load(
-        checkpoint_path, map_location="cuda" if torch.cuda.is_available() else "cpu"
-    )
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
     new_state_dict = {
         k.replace("_orig_mod.", ""): v for k, v in checkpoint.items()
     }  # Remove _orig_mod. prefix
